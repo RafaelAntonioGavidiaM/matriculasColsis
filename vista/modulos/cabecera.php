940e128e807a2618b1165d3958051cbfb7cd01c8
Edisson <eacamacho46@misena.edu.co>
2021-07-28 08:06:36 -0500

    Acepte Cambios

--- vista/modulos/cabecera.php
+++ vista/modulos/cabecera.php
@@ -22,70 +22,54 @@
 
 <body>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#myPage">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" i   d="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-
-
-      <?php
-
-      $permisos= $_SESSION["permisos"];
-
-      foreach ($permisos as $key => $value) {
-
-        if($value["idPermiso"]==1){
-
-          if($value["nombreFormulario"]=="facturaWeb" ){
-
-          }
-          else{
-            $rest = substr($value["nombreFormulario"], 3);  // devuelve "abcde"
-            $mayus=strtoupper($rest); //mayusculas
-            $minus=strtolower($rest); // minusculas
-  
-          echo "<li><a href=".$minus.">".$mayus."</a></li>";
-  
-          }
-
-
-        }
-
-        
-        
-      }
-       
-
-      
-
-
-
-      ?>
-
-      </ul>
-    </div>
-  </div>
-</nav>
-<br>
-
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#myPage">Logo</a>
       </div>
       <div class="collapse navbar-collapse" i d="myNavbar">
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="rol">ROL</a></li>
-          <li><a href="estudiante">ESTUDIANTE</a></li>
-          <li><a href="#portfolio">PORTFOLIO</a></li>
-          <li><a href="#pricing">PRICING</a></li>
-          <li><a href="#contact">CONTACT</a></li>
+
+
+          <?php
+
+          $permisos = $_SESSION["permisos"];
+
+          foreach ($permisos as $key => $value) {
+
+            if ($value["idPermiso"] == 1) {
+
+              if ($value["nombreFormulario"] == "facturaWeb") {
+              } else {
+                $rest = substr($value["nombreFormulario"], 3);  // devuelve "abcde"
+                $mayus = strtoupper($rest); //mayusculas
+                $minus = strtolower($rest); // minusculas
+
+                echo "<li><a href=" . $minus . ">" . $mayus . "</a></li>";
+              }
+            }
+          }
+
+
+
+
+
+
+          ?>
+
         </ul>
       </div>
     </div>
+  </nav>
+  <br>
+
+  </div>
+
+  </div>
   </nav>
   <br>
