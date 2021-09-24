cac86ade0c87d501134d536d8711b9ad073c0011
JimmyEuscategui <pulimomi2002@gmail.com>
2021-09-24 07:32:44 -0500

    24/09/2021

---
+++ vista/modulos/inicio.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container">
+        <div class="texto">
+            <div class="caja">
+                <h1 id="h1rf"></h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="wave" style="height: 150px; overflow: hidden;">
+        <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M-4.22,101.14 C240.12,148.52 305.58,54.77 503.10,122.86 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: rgb(235, 235, 235);"></path>
+        </svg>
+    </div>
+</header>
